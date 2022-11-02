f6d77368871d9d899e24ce309d285e63ca831ac0
JirkaKlimes <jiriklimes920@gmail.com>
2022-11-02 23:33:16 +0100

    added methods for building model

--- models.py
+++ models.py
@@ -1,7 +1,11 @@
+import numpy as np
+import random
+
 from abc import ABC, abstractmethod
 from gnn import Gnn
 from activations import Activation
 from losses import Loss
+
 
 class Model(ABC):
     def __init__(self, **kwargs):
@@ -30,7 +34,6 @@ class UnnamedModel1(Model):
 
         super().__init__()
 
-
     def __str__(self):
         return "Unnamed Model 1"
 
@@ -42,24 +45,51 @@ class UnnamedModel1(Model):
         # loss function
         self.loss_fn = None
 
-    def add_activation_function(self, function: Activation):
-        """
-        Adds funtion to a list of activations that can/will be used
-        """
-        if self._isBuilt:
-            print(f"Cannot add activation function to an already build network.")
-            return
+    def add_activation_functions(self, *funcs: Activation):
+        for function in funcs:
+            """
+            Adds funtion to a list of activations that can/will be used
+            """
+            if self._isBuilt:
+                raise Exception(f"Cannot add activation function to an already build network.")
 
-        if str(function) in list(map(str, self.activation_functions)):
-            print(f"Trainer already has \"{function}\" activation.")
-            return
+            if str(function) in list(map(str, self.activation_functions)):
+                raise Exception(f"Trainer already has \"{function}\" activation.")
 
-        self.activation_functions.append(function)
+            self.activation_functions.append(function)
+
+    def set_loss_function(self, function: Loss):
+        """
+        Sets a function that will be used to computer network error and gradient
+        """
+        self.loss_fn = function
+
+    def _fully_connect(self):
+        """
+        connected all inputs to all output neurons
+        like in regular dense layer
+        """
+        for o in range(self.gnn.N_inputs, self.gnn.N_inputs+self.gnn.N_outputs):
+            for i in range(self.gnn.N_inputs):
+                weight_index = self.gnn.add_connection(i, o)
+                self.gnn.weights[o, weight_index] = np.random.normal(size=(1))
+
+            self.gnn.activation_functions_ids[o] = random.randint(0, len(self.activation_functions)-1)
 
     def build(self):
+        if self.loss_fn is None:
+            raise Exception("Cannot build, model is missing loss function.")
+        self.gnn.loss_fn = self.loss_fn
+
+
+        if not self.activation_functions:
+            raise Exception("Cannot build, model doesn't have a single activation function.")
+        self.gnn.activation_functions = tuple(self.activation_functions)
+
+        self._fully_connect()
         self._isBuilt = True
 
-    def train(self, trainX, trainY, target_loss: float = 0):
+    def train(self, trainX, trainY, target_loss: float = 0.01):
         """
         Trains the network on dataset
         """
