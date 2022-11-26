df59536c9bd19fdca84449f999bbf54090c8ac8f
JirkaKlimes <93945030+JirkaKlimes@users.noreply.github.com>
2022-11-26 21:17:11 +0100

    Delete gnn-rust/target directory

--- gnn-rust/target/debug/deps/gnn_rust-a1eb812a5ad0cd24.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Jirka\Documents\Projects\Python\gnn\gnn-rust\target\debug\deps\gnn_rust-a1eb812a5ad0cd24.exe: src\main.rs
-
-C:\Users\Jirka\Documents\Projects\Python\gnn\gnn-rust\target\debug\deps\gnn_rust-a1eb812a5ad0cd24.d: src\main.rs
-
-src\main.rs:
--- gnn-rust/target/debug/gnn-rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Jirka\Documents\Projects\Python\gnn\gnn-rust\target\debug\gnn-rust.exe: C:\Users\Jirka\Documents\Projects\Python\gnn\gnn-rust\src\main.rs
