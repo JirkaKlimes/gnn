d48ec2c91a9050bbb0bcf2c37d78865ce45e7b81
JirkaKlimes <jiriklimes920@gmail.com>
2023-10-30 22:18:44 +0100

    changed structure a bit

--- gnn/src/gnn.py
+++ gnn/src/gnn.py
@@ -1,92 +1,148 @@
 import numpy as np
-from typing import List
+from typing import List, Optional
 from gnn.src.activations import Activation, Linear
 
 
-class SameOrderException(Exception):
-    def __init__(self, from_idx: int, to_idx: int, order: int):
-        message = f"Cannot connect 2 neurons ({from_idx} -> {to_idx}) with same order ({order})"
-        super().__init__(message)
+class CycleConnection(Exception):
+    def __init__(self, from_idx, to_idx) -> None:
+        msg = f"Cannot connect {from_idx} -> {to_idx} (would result in cycle)"
+        super().__init__(msg)
+
+
+class InputConnection(Exception):
+    def __init__(self, from_idx, to_idx) -> None:
+        msg = f"Cannot connect {from_idx} -> {to_idx} (is input)"
+        super().__init__(msg)
 
 
 class GNN:
     VALUE_DTYPE = np.float32
     INDEX_DTYPE = np.uint32
 
-    def __init__(
-        self,
-        n_inputs: int,
-        outputs: List[Activation],
-        value_dtype: np.dtype = VALUE_DTYPE,
-        index_dtype: np.dtype = INDEX_DTYPE
-    ):
-        self.n_inputs = n_inputs
-        self.n_outputs = len(outputs)
-        self.value_dtype = value_dtype
-        self.index_dtype = index_dtype
+    def __init__(self,
+                 n_inputs: int,
+                 outputs: List[Activation],
+                 value_dtype: np.dtype = VALUE_DTYPE,
+                 index_dtype: np.dtype = INDEX_DTYPE
+                 ):
+        self.n_in = n_inputs
+        self.n_out = len(outputs)
+        self.valueT = value_dtype
+        self.indexT = index_dtype
 
-        self.max_index = np.iinfo(self.index_dtype).max
+        self.max_index = np.iinfo(self.indexT).max
 
-        self.gnn_funcs = {Linear: 0}
+        self.funcs = {Linear: 0}
         list(map(self.add_activation, outputs))
 
-        self.neuron_order = np.full(
-            self.n_outputs, self.max_index, dtype=self.index_dtype
-        )
+        self.neuron_functions = np.uint8([self.funcs[f] for f in outputs])
+        self.neuron_biases = np.zeros(self.n_out, self.valueT)
+        self.multiplicative = np.zeros(self.n_out, bool)
 
-        self.neuron_functions = np.uint8([self.gnn_funcs[f] for f in outputs])
-        self.neuron_biases = np.zeros(self.n_outputs, self.value_dtype)
-        self.multiplicative = np.zeros(self.n_outputs, bool)
+        self.layer_sizes = np.full(1, self.n_out, self.indexT)
+        self.layer_pointers = np.zeros(1, self.indexT)
+        self.neuron_indices = np.arange(self.n_out, dtype=self.indexT)
+        self.neuron_layers = np.zeros(self.n_out, dtype=self.indexT)
 
-        self.weight_pointers = np.zeros(self.n_outputs, dtype=self.index_dtype)
-        self.weight_counts = np.zeros(self.n_outputs, dtype=self.index_dtype)
-        self.weights = np.empty(0, dtype=self.value_dtype)
-        self.connections = np.empty(0, dtype=self.index_dtype)
+        self.conn_counts = np.zeros(self.n_out, dtype=self.indexT)
+        self.conn_pointers = np.zeros(self.n_out, dtype=self.indexT)
+
+        self.conn_weights = np.empty(0, dtype=self.valueT)
+        self.conn_indices = np.empty(0, dtype=self.indexT)
+        self.conn_recurrent = np.empty(0, bool)
+
+    def __len__(self):
+        return self.multiplicative.size
+
+    @property
+    def n_neurons(self):
+        return len(self)
+
+    @property
+    def n_connections(self):
+        return self.conn_weights.size
 
     def add_activation(self, func: Activation):
-        if func not in self.gnn_funcs:
-            self.gnn_funcs[func] = len(self.gnn_funcs)
+        if func not in self.funcs:
+            self.funcs[func] = len(self.funcs)
 
-    def add_connection(self, from_idx: INDEX_DTYPE, to_idx: INDEX_DTYPE, weight: VALUE_DTYPE):
-        # offset of `n_inputs` since we don't store anything for input neurons
-        wit = to_idx - self.n_inputs
-        wif = from_idx - self.n_inputs
-
-        if from_idx >= self.n_inputs and self.neuron_order[wit] == self.neuron_order[wif]:
-            raise SameOrderException(wif, wit, self.neuron_order[wit])
-
-        self.weight_pointers[wit + 1:] += 1
-        p = self.weight_pointers[wit]
-        self.weights = np.insert(
-            self.weights, p + self.weight_counts[wit], weight)
-        self.connections = np.insert(
-            self.connections, p + self.weight_counts[wit], from_idx)
-        self.weight_counts[wit] += 1
-
-    def add_neuron(
-        self,
-        from_idx: INDEX_DTYPE, to_idx: INDEX_DTYPE, order: INDEX_DTYPE,
-        wf: VALUE_DTYPE, wt: VALUE_DTYPE, b: VALUE_DTYPE,
-        func: Activation, m: bool = False
-    ):
-        wit = to_idx - self.n_inputs
-        wif = from_idx - self.n_inputs
-
-        if (from_idx >= self.n_inputs and order == self.neuron_order[wif]) or \
-                order == self.neuron_order[wit]:
-            raise SameOrderException(wif, 'not-added', self.neuron_order[wif])
-
-        self.add_activation(func)
-
-        self.neuron_order = np.append(self.neuron_order, order)
+    def __add_unconnected_neuron(self, function: Activation, bias: VALUE_DTYPE, multiplicative: bool):
         self.neuron_functions = np.append(
-            self.neuron_functions, self.gnn_funcs[func])
-        self.neuron_biases = np.append(self.neuron_biases, b)
-        self.multiplicative = np.append(self.multiplicative, m)
-        self.weight_pointers = np.append(
-            self.weight_pointers, self.weights.size)
-        self.weight_counts = np.append(self.weight_counts, 0)
+            self.neuron_functions, self.funcs[function])
+        self.neuron_biases = np.append(self.neuron_biases, bias)
+        self.multiplicative = np.append(self.multiplicative, multiplicative)
 
-        middle_idx = self.n_inputs + self.neuron_order.size - 1
-        self.add_connection(from_idx, middle_idx, wf)
-        self.add_connection(middle_idx, to_idx, wt)
+        self.conn_counts = np.append(self.conn_counts, 0)
+        self.conn_pointers = np.append(self.conn_pointers, self.n_connections)
+
+    def add_neuron(self,
+                   from_idx: INDEX_DTYPE, from_weight: VALUE_DTYPE,
+                   to_idx: INDEX_DTYPE, to_weight: VALUE_DTYPE,
+                   function: Activation, bias: VALUE_DTYPE,
+                   multiplicative: bool,
+                   from_recurrent: Optional[bool] = False, to_recurrent: Optional[bool] = False,
+                   layer: Optional[INDEX_DTYPE] = None
+                   ):
+
+        if from_idx >= self.n_in and self.neuron_layers[from_idx - self.n_in] > self.neuron_layers[to_idx - self.n_in]:
+            raise CycleConnection(from_idx, to_idx)
+
+        if to_idx < self.n_in:
+            raise InputConnection(from_idx, to_idx)
+
+        mid_idx = len(self)
+        self.__add_unconnected_neuron(function, bias, multiplicative)
+        # finds layers between the 2 connecting neurons where new one could be inserted
+        lf = 0 if from_idx < self.n_in else \
+            self.neuron_layers[from_idx - self.n_in] + 1
+        lt = self.neuron_layers[to_idx - self.n_in] + 1
+        sizes = self.layer_sizes[lf: min(lt, self.layer_sizes.size - 1)]
+        if not sizes.size:
+            # if no layer found, create new
+            li = self.neuron_layers[to_idx - self.n_in]
+            self.layer_sizes = np.insert(self.layer_sizes, li, 1)
+            self.layer_pointers = np.insert(
+                self.layer_pointers, li, self.layer_pointers[li])
+            self.layer_pointers[li + 1:] += 1
+            self.neuron_indices = np.insert(self.neuron_indices, li, mid_idx)
+            self.neuron_layers = np.append(self.neuron_layers, li)
+            self.neuron_layers[self.layer_pointers[li:]] += 1
+        else:
+            # if layers found, pick the smallest one
+            li = np.argmin(sizes) + lf
+            self.neuron_indices = np.insert(
+                self.neuron_indices, self.layer_pointers[li] + self.layer_sizes[li], mid_idx)
+            self.layer_sizes[li] += 1
+            self.layer_pointers[li + 1:] += 1
+            self.neuron_layers = np.append(self.neuron_layers, li)
+
+        mid_idx += self.n_in
+        self.add_connection(from_idx, mid_idx, from_weight, from_recurrent)
+        self.add_connection(mid_idx, to_idx, to_weight, to_recurrent)
+
+    def add_connection(self,
+                       from_idx: INDEX_DTYPE, to_idx: INDEX_DTYPE,
+                       weight: VALUE_DTYPE, recurrent: Optional[bool] = False
+                       ):
+
+        if from_idx >= self.n_in and self.neuron_layers[from_idx - self.n_in] > self.neuron_layers[to_idx - self.n_in]:
+            raise CycleConnection(from_idx, to_idx)
+
+        if to_idx < self.n_in:
+            raise InputConnection(from_idx, to_idx)
+
+        if self.neuron_layers[from_idx - self.n_in] == self.neuron_layers[to_idx - self.n_in]:
+            # TODO: split layer in this case
+            raise NotImplementedError()
+
+        wit = to_idx - self.n_in
+
+        self.conn_pointers[wit + 1:] += 1
+        p = self.conn_pointers[wit]
+        self.conn_weights = np.insert(
+            self.conn_weights, p + self.conn_counts[wit], weight)
+        self.conn_indices = np.insert(
+            self.conn_indices, p + self.conn_counts[wit], from_idx)
+        self.conn_recurrent = np.insert(
+            self.conn_recurrent, p + self.conn_counts[wit], recurrent)
+        self.conn_counts[wit] += 1
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from gnn import GNN
 from gnn import ReLU, Linear
+from pyvis.network import Network
 
 n_inputs = 4
 outputs = [ReLU, Linear]
@@ -8,29 +9,50 @@ gnn = GNN(n_inputs, outputs)
 
 
 def print_gnn():
-    print('neuron_order:', gnn.neuron_order)
+    print('=' * 20)
     print('neuron_functions:', gnn.neuron_functions)
     print('neuron_biases:', gnn.neuron_biases)
     print('multiplicative:', gnn.multiplicative)
-    print('weight_pointers:', gnn.weight_pointers)
-    print('weight_counts:', gnn.weight_counts)
-    print('weights:', gnn.weights)
-    print('connections:', gnn.connections)
+    print('layer_sizes:', gnn.layer_sizes)
+    print('layer_pointers:', gnn.layer_pointers)
+    print('neuron_indices:', gnn.neuron_indices)
+    print('neuron_layers:', gnn.neuron_layers)
+    print('conn_pointers:', gnn.conn_pointers)
+    print('conn_counts:', gnn.conn_counts)
+    print('conn_weights:', gnn.conn_weights)
+    print('conn_indices:', gnn.conn_indices)
+    print('conn_recurrent:', gnn.conn_recurrent)
+
+
+def export_gnn():
+    net = Network(
+        notebook=True, directed=True,
+        neighborhood_highlight=True,
+        width='100%', height='900',
+        cdn_resources='in_line'
+    )
+    net.barnes_hut()
+
+    for i in range(gnn.n_in):
+        net.add_node(i, color='#57dc88', label=str(i))
+
+    for i in range(gnn.n_in, gnn.n_in + gnn.n_out):
+        net.add_node(i, color='#f45b5c', label=str(i))
+
+    for i in range(gnn.n_in + gnn.n_out, gnn.n_in + len(gnn)):
+        net.add_node(i, color='#4ad0df', label=str(i))
+
+    for i, (p, c) in enumerate(zip(gnn.conn_pointers, gnn.conn_counts)):
+        for j in range(p, p + c):
+            f = gnn.conn_indices[j]
+            net.add_edge(int(f), i + gnn.n_in)
+
+    net.save_graph('gnn.html')
 
 
 print_gnn()
-gnn.add_connection(0, 4, 0.1)
-print('=' * 10)
+gnn.add_neuron(0, 0.2, 4, -0.3, ReLU, 1, True)
 print_gnn()
-gnn.add_connection(2, 4, 0.2)
-print('=' * 10)
+gnn.add_neuron(0, 0.7, 4, 1.1, ReLU, 1, False)
 print_gnn()
-gnn.add_connection(1, 5, -0.4)
-print('=' * 10)
-print_gnn()
-gnn.add_connection(3, 5, 0.5)
-print('=' * 10)
-print_gnn()
-gnn.add_neuron(0, 5, gnn.max_index // 2, 0.3, 0.7, 0, ReLU, True)
-print('=' * 10)
-print_gnn()
+export_gnn()
